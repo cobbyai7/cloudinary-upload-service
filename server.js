35d18488610187f87cbf7e67d7d34776310cda8d
cobbyai7 <cobbyai07@gmail.com>
2026-05-27 12:59:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+import express from 'express';
+import uploadImageRoute from './routes/uploadImage.js';
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/upload-image', uploadImageRoute);
+
+app.get('/', (req, res) => {
+  res.send('Cloudinary Upload Service Running');
+});
+
+app.listen(process.env.PORT || 10000, () =>
+  console.log('Server running...')
+);
 
